b71b872c8de1b8dc806cb1c960189eea87d6da19
ARUNAVA DE <93212198+arunavade99@users.noreply.github.com>
2025-11-10 20:29:31 +0530

    Change YOLO model file to 'best38.pt'

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy
 import math
 ultralytics.checks()
 # model = YOLO('yolov8n.pt')
-model = YOLO('best.pt')
+model = YOLO('best38.pt')
 
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('816.mp4')
